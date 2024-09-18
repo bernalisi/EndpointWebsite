f87c44474954a100637a31640f71b91235f9abc2
Joshua Smith <sarjsmithde@gmail.com>
2024-09-18 18:26:40 +0200

    last time please?

--- src/components/life-sciences/UseCases.jsx
+++ src/components/life-sciences/UseCases.jsx
@@ -25,7 +25,7 @@ export default function Why() {
   };
 
   return (
-    <div className="w-full h-auto ld:pb-10 lg:h-[1350px] ld:h-[1900px] bg-white flex flex-col justify-start xl:justify-start items-start gap-4 px-4 sm:px-10 lg:px-14 pt-10 sm:pt-16 xl:mt-10">
+    <div className="w-full h-auto pb-20 ld:pb-10 ld:h-[2100px] bg-white flex flex-col justify-start xl:justify-start items-start gap-4 px-4 sm:px-10 lg:px-14 pt-10 sm:pt-16 xl:mt-10">
       <SectionSeparator TitleSection="EXPLORE OUR USE CASES" />
 
       <h3 className="text-black text-[28px] lg:text-[48px] font-semibold mt-3 py-[20px]">
@@ -37,31 +37,39 @@ export default function Why() {
         datasets can answer research questions across the product lifecycle.
       </p>
 
-      <div className="flex justify-start gap-10 w-full">
+      {/* Chevron Navigation for Small Screens */}
+      <div className="w-full flex justify-between items-center xl:hidden">
+        <button onClick={handlePrev}>
+          <LuChevronLeft size={24} />
+        </button>
+        <span className="text-lg font-semibold">
+          {useCaseNavigation[defaultUseCase].title}
+        </span>
+        <button onClick={handleNext}>
+          <LuChevronRight size={24} />
+        </button>
+      </div>
+
+      <div className="flex flex-col xl:flex-row justify-start gap-10 w-full">
         {/* Navigation - Hidden on small screens */}
-        <div className="w-full xl:w-[15%] hidden xl:flex mt-10 flex-wrap xl:mt-20 flex-row xl:flex-col justify-start text-left items-start ">
+        <div className="w-full hidden xl:flex xl:w-[15%] mt-10 flex-wrap xl:mt-20 flex-row xl:flex-col justify-start text-left items-start ">
           {Object.keys(useCaseNavigation).map((key) => (
             <button
               key={key}
               onClick={() => setDefaultUseCase(Number(key))}
               className={
                 defaultUseCase === Number(key)
-                  ? "border-l-4 ld:text-[20px] border-violet-900 text-left text-violet-900 py-4 px-2 sm:px-4 w-full text-sm sm:text-base"
-                  : "border-l-4 ld:text-[20px] hover:border-violet-900 text-left hover:text-violet-900 py-4 px-2 sm:px-4 w-full text-sm sm:text-base"
+                  ? "border-l-4 xl:border-b-0 ld:text-[20px] border-violet-900 text-left text-violet-900 py-4 px-2 sm:px-4 w-full text-sm sm:text-base"
+                  : "border-l-4 xl:border-b-0 ld:text-[20px] hover:border-violet-900 text-left hover:text-violet-900 py-4 px-2 sm:px-4 w-full text-sm sm:text-base"
               }
             >
               {useCaseNavigation[key].title}
             </button>
           ))}
         </div>
+
         {/* Use Case Content */}
-        <button className="xl:hidden" onClick={handlePrev}>
-          <LuChevronLeft alt="Previous" className="h-6 w-6" />
-        </button>
         <UseCase defaultUseCase={defaultUseCase} />
-        <button className="xl:hidden" onClick={handleNext}>
-          <LuChevronRight alt="Next" className="h-6 w-6" />
-        </button>
       </div>
     </div>
   );
